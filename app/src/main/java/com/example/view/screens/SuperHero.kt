139ebe2efe5e05c6361735f0c681a13dc9cba1ec
eloymartos <115500079+eloymartos@users.noreply.github.com>
2023-11-24 18:27:31 +0100

    hasta punto 8

--- app/src/main/java/com/example/view/screens/SuperHero.kt
+++ app/src/main/java/com/example/view/screens/SuperHero.kt
@@ -1,13 +1,17 @@
 package com.example.view.screens
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -17,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.view.R
@@ -36,7 +41,7 @@ fun getSuperHeroes():MutableList<Superhero>{
 
 @Composable
 fun SuperHeroView() {
-    LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
         items(getSuperHeroes()) {
             ItemHero(superhero = it)
         }
@@ -45,12 +50,18 @@ fun SuperHeroView() {
 
 @Composable
 fun ItemHero(superhero: Superhero){
+    val context = LocalContext.current
+    val mostrarMensaje : ()->Unit = {
+        Toast.makeText(context, "${superhero.realName}", Toast.LENGTH_SHORT).show()
+    }
     Card (
         Modifier
             .border(2.dp, Color.Red)
-            .size(width = 200.dp, height = 270.dp)
+            .fillMaxWidth()
         ){
-        Column (verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally){
+        Column (verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.clickable { mostrarMensaje() }){
             Image(painter = painterResource(id = superhero.photo),
                 contentDescription = "foto de ${superhero.superHeroName}",
                 contentScale = ContentScale.Crop,
@@ -58,7 +69,7 @@ fun ItemHero(superhero: Superhero){
             )
             Text(superhero.superHeroName)
             Text(superhero.realName)
-            Text(text = superhero.publisher, Modifier.padding(start = 130.dp))
+            Text(text = superhero.publisher, Modifier.padding(start = 260.dp))
         }
 
     }
